4eaee6bf0b58d3444470f950e2ca11292f3588c3
abdallah <abdallahantony55.aa@gmail.com>
2022-12-12 17:07:58 +0300

    combine reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import {reducer as books} from './books/books';
-import {reducer as categories} from './categories/categories';
+import books from './books/books';
+import categories from './categories/categories';
 
 const configureStore = () => combineReducers({ books, categories });
 
