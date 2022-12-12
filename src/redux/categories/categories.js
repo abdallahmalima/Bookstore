765efd44aae2904f82c6a915fbd536b53c2ee9c1
abdallah <abdallahantony55.aa@gmail.com>
2022-12-12 18:13:54 +0300

    Add duck patterns to category.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,6 +1,6 @@
-const CHECK_STATUS = 'CHECK_STATUS';
+const CHECK_STATUS = 'book-store/categories/CHECK_STATUS';
 
-const categories = (state = [], action = {}) => {
+const reducer = (state = [], action = {}) => {
   switch (action.type) {
     case CHECK_STATUS:
       return 'Under construction';
@@ -13,4 +13,4 @@ export const statusChecker = () => ({
   type: CHECK_STATUS,
 });
 
-export default categories;
+export default reducer;
