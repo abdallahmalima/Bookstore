189b21a8ea68dd6497e248f70a8d07fc19a61427
abdallah <abdallahantony55.aa@gmail.com>
2023-01-01 19:01:33 +0300

    validate field by trim

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -9,6 +9,10 @@ const BookForm = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [category, setCategory] = useState('');
+  const [errorTitle, setErrorTitle] = useState(null);
+  const [errorAuthor, setErrorAuthor] = useState(null);
+  const [errorCategory, setErrorCategory] = useState(null);
+
   const dispatch = useDispatch();
 
   const { deletedError } = useSelector((state) => state.books);
@@ -25,12 +29,37 @@ const BookForm = () => {
     setAuthor('');
     setCategory('');
   };
+  const isValid = (title, author, category) => {
+    let validStatus = true;
+    if (title.length === 0) {
+      setErrorTitle('Title field is required');
+      validStatus = false;
+    }
+    if (author.length === 0) {
+      setErrorAuthor('Author field is required');
+      validStatus = false;
+    }
+
+    if (category.length === 0) {
+      setErrorCategory('Category field is required');
+      validStatus = false;
+    }
+
+    return validStatus;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const mTitle = title.trim();
+    const mAuthor = author.trim();
+
+    if (!isValid(mTitle, mAuthor, category)) {
+      return;
+    }
+
     dispatch(createBook({
-      item_id: uuidv4(), title, author, category,
+      item_id: uuidv4(), title: mTitle, author: mAuthor, category,
     }));
     resetForm();
   };
@@ -53,6 +82,7 @@ const BookForm = () => {
             placeholder="Book Title"
             required
           />
+          {errorTitle && <span className="text-red-700">*{errorTitle}*</span>}
         </label>
         <label
           htmlFor="author"
@@ -67,6 +97,7 @@ const BookForm = () => {
             placeholder="Book Author"
             required
           />
+           {errorAuthor && <span className="text-red-700">*{errorAuthor}*</span>}
         </label>
         <label
           htmlFor="category"
@@ -121,6 +152,7 @@ const BookForm = () => {
               Historical fiction
             </option>
           </select>
+          {errorCategory && <span className="text-red-700">*{errorCategory}*</span>}
         </label>
         <button type="submit" className="text-dirty-white app-bg-primary rounded w-fit font-light font-roboto px-12 py-3 text-base font-bold">ADD BOOK</button>
       </div>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -13,6 +13,7 @@ const BookList = () => {
   return <div className="flex flex-col gap-12 items-center w-fit m-auto md:w-full  md:flex-col justify-between py-8">
     {loading && <h3 className="textError">Loading...</h3>}
     {error && <h3 className="textError">{error}</h3>}
+    {!loading && !error && (books.length === 0) && <h3 className="textError">Please Add Book</h3>}
     {!loading && !error && books.map((book) => (
       <Book key={book.id} {...book} />
     ))}
